d2b0d4de278a1f830fd9aa21294e1919d61347db
ufocombat <ufocombat@gmail.com>
2017-11-03 13:45:42 +0300

    temp+led

--- WiFi-AM2301-D1.ino
+++ WiFi-AM2301-D1.ino
@@ -1,11 +1,13 @@
 #include "DHT.h"
 #define DHTPIN D7
+#define LED_SCL D5
 #define DHTTYPE DHT21
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(LED_SCL, OUTPUT);   
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
+  Serial.println("DHT21 Vers 1.01");
 
   dht.begin();
 }
@@ -14,26 +16,20 @@ void loop() {
   delay(2000);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  float hif = dht.computeHeatIndex(f, h);
-  float hic = dht.computeHeatIndex(t, h, false);
+  
+  digitalWrite(LED_SCL, HIGH);
+  delay(600);
+  digitalWrite(LED_SCL, LOW);
 
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  Serial.println(" *C ");
 }
 
