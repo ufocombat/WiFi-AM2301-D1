953e0a4ad0ce1faeb78333b394d446e97f175590
ufocombat <ufocombat@gmail.com>
2018-03-27 18:07:59 +0300

    ++Fun

--- WiFi-AM2301-D1.ino
+++ WiFi-AM2301-D1.ino
@@ -5,8 +5,8 @@
 #define DHTTYPE DHT21
 DHT dht(DHTPIN, DHTTYPE);
 
-const String  vers = "2.42";
-const char* host = "ufoiot.azurewebsites.net";
+const String  vers = "3.3";
+const char* host = "ufoiot2.azurewebsites.net";
 const int httpPort = 80;
 
 /*const char* ssid[]     = {"Enceladus","Astra"};
@@ -18,55 +18,10 @@ const char* password[] = {"20011983"};
 int dell = 10000;
 String _ssid="";
 
-void WiFiConnect(){
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    for (int i=0;i<2;i++)
-    {
-      Serial.println();
-      Serial.print("Connecting to ");
-      Serial.println(ssid[i]);
-      
-      WiFi.begin(ssid[i], password[i]);
-      _ssid = ssid[i];
-    
-      int tr = 0;
-  
-        
-      while (WiFi.status() != WL_CONNECTED) 
-      {
-        delay(500);
-        Serial.print(".");
-        if ((i==0)&(tr++>20)) break;
-      }
-  
-      if (WiFi.status() == WL_CONNECTED)
-      {
-        Serial.println("");
-        Serial.println("WiFi connected");  
-          
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());  
-        break;
-      }
-      else
-      {
-        Serial.println("");
-        Serial.println("failed connection!");  
-      }    
-    }
-  }
-}
-
 void setup() {
-  pinMode(LED_SCL, OUTPUT);   
-  
-  Serial.begin(9600);
-  delay(100);
-  
-  Serial.println("DHT21 "+vers);  
-  dht.begin();
-  
+  pinMode(LED_SCL, OUTPUT);     
+  delay(300);
+  dht.begin();  
 }
 
 int value = 0;
@@ -79,28 +34,65 @@ void loop() {
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    //Serial.println("Failed to read from DHT sensor!");
     return;
   }
   
-  Serial.print("Humidity: ");
+ /* Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.println(" *C ");
 
-///
 
   Serial.print("connecting to ");
-  Serial.println(host);
+  Serial.println(host);*/
 
-  WiFiConnect();
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    for (int i=0;i<2;i++)
+    {
+      /*Serial.println();
+      Serial.print("Connecting to ");
+      Serial.println(ssid[i]);*/
+      
+      WiFi.begin(ssid[i], password[i]);
+      _ssid = ssid[i];
+    
+      int tr = 0;
+  
+        
+      while (WiFi.status() != WL_CONNECTED) 
+      {
+        delay(500);
+       // Serial.print(".");
+        if ((i==0)&(tr++>20)) break;
+      }
+  
+      if (WiFi.status() == WL_CONNECTED)
+      {
+     /*   Serial.println("");
+        Serial.println("WiFi connected");  
+          
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());  */
+        break;
+      }
+      else
+      {
+       /* Serial.println("");
+        Serial.println("failed connection!");  */
+      }    
+    }
+  }
+
+
   WiFiClient client;
   
   if (!client.connect(host, httpPort)) 
   {
-    Serial.println("connection failed");
+   // Serial.println("connection failed");
     return;
   }
 
@@ -108,15 +100,16 @@ void loop() {
 
 // Settings request
 //  String url = "http://ufoiot.azurewebsites.net/settings";
-   String url = "https://ufoiot2.azurewebsites.net/api/settings";  
+   String url = "https://ufoiot2.azurewebsites.net/api/settings?vers=";  
+   url += vers;
 /*  url += streamId;
   url += "?private_key=";
   url += privateKey;
   url += "&value=";
   url += value;*/
   
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+ /* Serial.print("Requesting URL: ");
+  Serial.println(url);*/
   
   client.println(String("GET ")+url+" HTTP/1.1");
   client.println("Host: ufoiot2.azurewebsites.net");
@@ -126,7 +119,7 @@ void loop() {
   
   while (client.available() == 0) {
     if (millis() - timeout > 10000) {
-      Serial.println("<Client Timeout !>");
+   //   Serial.println("<Client Timeout !>");
       client.stop();
       return;
     }
@@ -144,35 +137,30 @@ void loop() {
       int w2 = line.indexOf("'}",w1+8);
       String w3 = line.substring(w1+8,w2);
       dell = w3.toInt();
-      Serial.print("Setting dellay to ");
-      Serial.println(dell);
+   /*   Serial.print("Setting dellay to ");
+      Serial.println(dell);*/
     }
   }
 
   if (!client.connect(host, httpPort)) 
   {
-    Serial.println("connection failed");
     return;
   }
   
-  url = "http://ufoiot.azurewebsites.net/weather";
-  
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+  //url = "http://ufoiot.azurewebsites.net/weather";
+  url = "https://ufoiot2.azurewebsites.net/api/weather?code=hQ47IbWcDemZbEiaEW/7qJOmvHPcphctAkfYuy3mY2NKnThtynn3/g==";
   
   String ss = "{";
   ss+="\"ID\":\"GEO-02\",";
   ss+="\"Temperature\":\""+String(t)+"\",";
   ss+="\"Humidity\":\""+String(h)+"\",";
   ss+="\"Term\":\"0\",";
-  ss+="\"ssid\":\""+_ssid+"\",\"Version\":\""+vers+"\"";
+  ss+="\"SSID\":\""+_ssid+"\",\"Version\":\""+vers+"\"";
   ss+="}";
-
-  Serial.print("POST: ");
-  Serial.println(ss);
-
-  client.println("POST /weather HTTP/1.1");
-  client.println("Host: ufoiot.azurewebsites.net");
+    
+  client.println(String("POST ")+url+" HTTP/1.1");  
+//  client.println("POST /api/weather HTTP/1.1");
+  client.println("Host: ufoiot2.azurewebsites.net");
   client.println("Cache-Control: no-cache");
   client.println("Content-Type: application/json");
   client.print("Content-Length: ");
@@ -183,7 +171,7 @@ void loop() {
   while (client.available() == 0) 
   {
     if (millis() - timeout > 10000) {
-      Serial.println("Client Timeout !");
+   //   Serial.println("Client Timeout !");
       client.stop();
       digitalWrite(LED_SCL, LOW);
       return;
@@ -192,11 +180,11 @@ void loop() {
 
   while(client.available()){
     String line = client.readStringUntil('\r');
-    Serial.print(line);
+  //  Serial.print(line);
   }
   
-  Serial.println();
-  Serial.println("closing connection");    
+ /* Serial.println();
+  Serial.println("closing connection");    */
 
   digitalWrite(LED_SCL, LOW);  
 }
